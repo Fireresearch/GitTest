d8fa443a625fc4636ccfc75125837be5c82bc057
Fireresearch <fireresearch789@gmail.com>
2019-01-10 12:27:17 +0530

    commit from system 2

---
+++ app/src/main/java/com/example/administrator/gittest/SecondClass.java
@@ -0,0 +1,5 @@
+package com.example.administrator.gittest;
+
+public class SecondClass {
+    // class created from system 2
+}
--- app/src/main/java/com/example/administrator/gittest/TestClass.java
+++ app/src/main/java/com/example/administrator/gittest/TestClass.java
@@ -5,4 +5,5 @@ package com.example.administrator.gittest;
  */
 
 public class TestClass {
+
 }
